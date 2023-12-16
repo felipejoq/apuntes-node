9553b0b678019c67c0bfd4a4c3ed6a83c213dc84
felipejoq <felipejofreq@gmail.com>
2023-12-16 19:04:12 -0300

    Fin sección 16

--- 10-restserver-postgres-clean-architecture/src/app.ts
+++ 10-restserver-postgres-clean-architecture/src/app.ts
@@ -2,9 +2,6 @@ import { envs } from './config/envs';
 import { AppRoutes } from './presentation/routes';
 import { Server } from './presentation/server';
 
-
-
-
 (async()=> {
   main();
 })();
--- 10-restserver-postgres-clean-architecture/src/presentation/server.ts
+++ 10-restserver-postgres-clean-architecture/src/presentation/server.ts
@@ -8,7 +8,6 @@ interface Options {
   public_path?: string;
 }
 
-
 export class Server {
 
   private app = express();
@@ -23,33 +22,27 @@ export class Server {
     this.routes = routes;
   }
 
-  
-  
   async start() {
-    
 
     //* Middlewares
-    this.app.use( express.json() ); // raw
-    this.app.use( express.urlencoded({ extended: true }) ); // x-www-form-urlencoded
-    this.app.use( compression() )
+    this.app.use(express.json()); // raw
+    this.app.use(express.urlencoded({ extended: true })); // x-www-form-urlencoded
+    this.app.use(compression())
 
     //* Public Folder
-    this.app.use( express.static( this.publicPath ) );
-
+    this.app.use(express.static(this.publicPath));
 
     //* Routes
-    this.app.use( this.routes );
-
+    this.app.use(this.routes);
 
     //* SPA
     this.app.get('*', (req, res) => {
-      const indexPath = path.join( __dirname + `../../../${ this.publicPath }/index.html` );
+      const indexPath = path.join(__dirname + `../../../${this.publicPath}/index.html`);
       res.sendFile(indexPath);
     });
-    
 
     this.app.listen(this.port, () => {
-      console.log(`Server running on port ${ this.port }`);
+      console.log(`Server running on port ${this.port}`);
     });
 
   }
