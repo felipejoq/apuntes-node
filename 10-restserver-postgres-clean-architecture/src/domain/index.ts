d7a10361791fab0dcc518ce8506b50ce6d1284ad
felipejoq <felipejofreq@gmail.com>
2023-12-16 18:57:01 -0300

    Fin sección 16

---
+++ 10-restserver-postgres-clean-architecture/src/domain/index.ts
@@ -0,0 +1,10 @@
+export * from './datasources';
+export * from './dtos';
+export * from './entities';
+export * from './repositories';
+
+export * from './use-cases/todo/create-todo';
+export * from './use-cases/todo/update-todo';
+export * from './use-cases/todo/delete-todo';
+export * from './use-cases/todo/get-todo';
+export * from './use-cases/todo/get-todos';
