f875303247d181011faba58b85cd2d848112ce7f
felipejoq <felipejofreq@gmail.com>
2023-12-19 16:48:14 -0300

    fin sección 25

--- 14-serverless-webhooks-node/netlify/functions/github-discord/github-discord.ts
+++ 14-serverless-webhooks-node/netlify/functions/github-discord/github-discord.ts
@@ -1,86 +1,86 @@
 import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
 
-const notify = async ( message: string ) => {
+const notify = async (message: string) => {
 
   const body = { content: message };
 
-  const resp = await fetch( process.env.DISCORD_WEBHOOK_URL ?? '', {
+  const resp = await fetch(process.env.DISCORD_WEBHOOK_URL ?? '', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify( body ),
-  } );
+    body: JSON.stringify(body),
+  });
 
-  if ( !resp.ok ) {
-    console.log( 'Error sending message to discord' );
+  if (!resp.ok) {
+    console.log('Error sending message to discord');
     return false;
   }
   return true;
 };
 
-const onStar = ( payload: any ): string => {
+const onStar = (payload: any): string => {
 
   const { action, sender, repository } = payload;
 
-  return `User ${ sender.login } ${ action } star on ${ repository.full_name }`;
+  return `User ${sender.login} ${action} star on ${repository.full_name}`;
 
 }
 
-const onIssue = ( payload: any ): string => {
+const onIssue = (payload: any): string => {
 
   const { action, issue } = payload;
 
-  if ( action === 'opened' ) {
-    return `An issue was opened with this title ${ issue.title }`;
+  if (action === 'opened') {
+    return `An issue was opened with this title ${issue.title}`;
   }
 
-  if ( action === 'closed' ) {
-    return `An issue was closed by ${ issue.user.login }`;
+  if (action === 'closed') {
+    return `An issue was closed by ${issue.user.login}`;
   }
 
-  if ( action === 'reopened' ) {
-    return `An issue was reopened by ${ issue.user.login }`;
+  if (action === 'reopened') {
+    return `An issue was reopened by ${issue.user.login}`;
   }
 
 
-  return `Unhandled action for the issue event ${ action }`;
+  return `Unhandled action for the issue event ${action}`;
 
 
 }
 
 
 
-const handler: Handler = async ( event: HandlerEvent, context: HandlerContext ) => {
+const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
 
-  const githubEvent = event.headers[ 'x-github-event'] ?? 'unknown';
-  const payload = JSON.parse( event.body ?? '{}' );
+  const githubEvent = event.headers['x-github-event'] ?? 'unknown';
+  const payload = JSON.parse(event.body ?? '{}');
   let message: string;
 
   console.log(payload);
 
-  switch( githubEvent ) {
+  switch (githubEvent) {
 
     case 'star':
-      message = onStar( payload );
-    break;
+      message = onStar(payload);
+      break;
 
     case 'issues':
       message = onIssue(payload);
-    break;
+      break;
 
 
     default:
-      message = `Unknown event ${ githubEvent }`;
+      message = `Unknown event ${githubEvent}`;
 
   }
 
 
-  await notify( message );
+  await notify(message);
 
   return {
     statusCode: 200,
-    body: JSON.stringify( {
+    body: JSON.stringify({
       message: 'done',
-    } ),
+    }),
     headers: {
       'Content-Type': 'application/json'
     }
